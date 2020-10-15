48aa2739fa45c33aabad71c718b00c749605bff8
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-15 20:37:22 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,8 +1,11 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      
-      }, 
+    :montague => { 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   }, 
     :capulet => {
       #your key/value pairs here!
       }}
